ab01c3bdc3999973254d561e67647f38bb5c3d5f
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-08-04 19:36:01 +0700

    init routes

--- server-dist/routes/web.php
+++ server-dist/routes/web.php
@@ -14,3 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'index');
+
+Route::name('user.')->group(function() {
+    Route::view('/stats', 'stats')->middleware(middleware: 'auth')->name(name: 'stats');
+    Route::get('/login', function() {
+        if(Auth::check()) {
+            return redirect(route(name: 'user.stats'));
+        }
+        return view(view: 'login');
+    })->name(name: 'login');
+
+    //Route::post('/login', []);
+
+    //Route::get('/logout', [])->name(name: 'logout');
+    Route::get('/register', function() {
+        if(Auth::check()) {
+            return redirect(route(name: 'user.stats'));
+        }
+        return view(name(name: 'register'));
+    })->name(name: 'register');
+
+    //Route::post('/register', []);
+});
